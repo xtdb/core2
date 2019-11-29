5cae23da672674411b9e17f42d2a13c372be1412
Håkan Råberg <hakan.raberg@gmail.com>
2019-11-29 21:02:34 +0100

    Adding flag constants to Java ArrowArray.

--- examples/arrow-jnr/src/crux/ArrowArray.java
+++ examples/arrow-jnr/src/crux/ArrowArray.java
@@ -6,6 +6,10 @@ import jnr.ffi.Struct;
 import jnr.ffi.annotations.Delegate;
 
 public class ArrowArray extends Struct {
+    public static final int ARROW_FLAG_ORDERED = 1;
+    public static final int ARROW_FLAG_NULLABLE = 2;
+    public static final int ARROW_FLAG_KEYS_SORTED = 4;
+
     public ArrowArray(Runtime runtime) {
         super(runtime);
     }

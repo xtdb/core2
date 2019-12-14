27520e7082d07cf1c9c981791cf2d1c142dfaa7d
Håkan Råberg <hakan.raberg@gmail.com>
2019-12-14 18:20:40 +0100

    Some links.

--- src/arrow_abi.rs
+++ src/arrow_abi.rs
@@ -1,3 +1,9 @@
+// See https://github.com/apache/arrow/pull/6026/files
+
+// cpp/src/arrow/c/abi.h
+// cpp/src/arrow/c/bridge.h
+// cpp/src/arrow/c/bridge.cc
+
 // // Licensed to the Apache Software Foundation (ASF) under one
 // // or more contributor license agreements.  See the NOTICE file
 // // distributed with this work for additional information

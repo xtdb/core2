b6439aff94968542371d1b99c380244d1d823996
Håkan Råberg <hakan.raberg@gmail.com>
2020-01-08 20:14:11 +0000

    Adding a few useful deps for spiking.

--- examples/arrow-jnr/project.clj
+++ examples/arrow-jnr/project.clj
@@ -8,6 +8,8 @@
                  [org.clojure/tools.logging "0.5.0"]
                  [com.github.jnr/jnr-ffi "2.1.9"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [org.apache.arrow/arrow-vector "0.15.1"]]
+                 [org.apache.arrow/arrow-vector "0.15.1"]
+                 [org.agrona/agrona "1.1.0"]
+                 [org.roaringbitmap/RoaringBitmap "0.8.12"]]
   :java-source-paths ["src"]
   :global-vars {*warn-on-reflection* true})

5d82704fc82e0180136e75f3be916300a7a1a148
Håkan Råberg <hakan.raberg@gmail.com>
2020-01-10 18:07:03 +0000

    Moving z-curve stuff to bottom for now.

--- examples/arrow-jnr/src/crux/wcoj.clj
+++ examples/arrow-jnr/src/crux/wcoj.clj
@@ -5,99 +5,7 @@
             [crux.datalog])
   (:import [clojure.lang IPersistentCollection IPersistentMap Repeat]))
 
-;; Simplistic spike using binary strings for z-order to
-;; explore the algorithms described in the papers:
-
-;; Optimal Joins using Compact Data Structures
-;; https://arxiv.org/abs/1908.01812
-;; Worst-Case Optimal Radix Triejoin
-;; https://arxiv.org/abs/1912.12747
-;; https://brodyf.github.io/thesis.pdf
-
 (set! *unchecked-math* :warn-on-boxed)
-
-(def ^:dynamic ^{:tag 'long} *binary-str-length* Long/SIZE)
-
-(defn ->binary-str ^String [^long i]
-  (str/replace
-   (format (str "%" *binary-str-length* "s") (Long/toUnsignedString i 2))
-   \space \0))
-
-(defn parse-binary-str ^Number [^String b]
-  (if (> (count b) Long/SIZE)
-    (BigInteger. b 2)
-    (Long/parseUnsignedLong b 2)))
-
-(defn dimension ^long [^String b]
-  (quot (count b) *binary-str-length*))
-
-(defn interleave-bits ^String [bs]
-  (->> (for [b bs]
-         (cond-> b
-           (int? b) (->binary-str)))
-       (apply interleave)
-       (str/join)))
-
-(defn as-path
-  ([^String b]
-   (as-path b (dimension b)))
-  ([^String b ^long d]
-   (map str/join (partition d b))))
-
-(defn select-in-path [p idxs]
-  (for [p p]
-    (str/join (map (vec p) idxs))))
-
-(defn component [^String b ^long idx]
-  (str/join (take-nth (dimension b) (drop idx b))))
-
-(defn components [^String b idxs]
-  (for [i idxs]
-    (component b i)))
-
-;; An example database for this query on domain {0, 1, 2} is R(A, B) =
-;; {(0, 1)}, S(B,C) = {(1, 2)}, T(A, C) = {(0, 2)} and a possible
-;; Booleanisation of the relations could be R(2) (A0, A1, B0, B1) =
-;; {(0, 0, 0, 1)}, S(2) (B0, B1 , C0, C1) = {(0, 1, 1, 0)}, T(2)(A0,
-;; A1, C0, C1) = {(0, 0, 1, 0)}
-
-(comment
-  ;; bits
-  (let [r (interleave-bits [0 1])
-        s (interleave-bits [1 2])
-        t (interleave-bits [0 2])
-        expected-q (interleave-bits [0 1 2])
-
-        find-vars '[A B C]
-        joins [['A [[r 0]
-                    [t 0]]]
-               ['B [[r 1]
-                    [s 0]]]
-               ['C [[s 1]
-                    [t 1]]]]
-
-        result-tuple (for [[_ join] joins
-                           :let [vs (for [[rel col] join]
-                                      (component rel col))]
-                           :while (apply = vs)]
-                       (first vs))
-        result (when (= (count find-vars)
-                        (count result-tuple))
-                 (interleave-bits result-tuple))]
-    [expected-q
-     result
-     (= expected-q result)
-     (->> (range (count find-vars))
-          (components result)
-          (mapv parse-binary-str))
-     find-vars
-     joins
-     (s/conform :crux.datalog/program
-                '[r(0, 1).
-                  s(1, 2).
-                  t(0, 2).
-
-                  q(A, B, C) :- r(A, B), s(B, C), t(A, C).])]))
 
 ;; generic-join
 
@@ -256,3 +164,95 @@
         db (compile-datalog db edge)
         result (query-datalog db 'path)]
     (= #{[1 2] [2 3] [1 3]} (set result))))
+
+;; Simplistic spike using binary strings for z-order to
+;; explore the algorithms described in the papers:
+
+;; Optimal Joins using Compact Data Structures
+;; https://arxiv.org/abs/1908.01812
+;; Worst-Case Optimal Radix Triejoin
+;; https://arxiv.org/abs/1912.12747
+;; https://brodyf.github.io/thesis.pdf
+
+(def ^:dynamic ^{:tag 'long} *binary-str-length* Long/SIZE)
+
+(defn ->binary-str ^String [^long i]
+  (str/replace
+   (format (str "%" *binary-str-length* "s") (Long/toUnsignedString i 2))
+   \space \0))
+
+(defn parse-binary-str ^Number [^String b]
+  (if (> (count b) Long/SIZE)
+    (BigInteger. b 2)
+    (Long/parseUnsignedLong b 2)))
+
+(defn dimension ^long [^String b]
+  (quot (count b) *binary-str-length*))
+
+(defn interleave-bits ^String [bs]
+  (->> (for [b bs]
+         (cond-> b
+           (int? b) (->binary-str)))
+       (apply interleave)
+       (str/join)))
+
+(defn as-path
+  ([^String b]
+   (as-path b (dimension b)))
+  ([^String b ^long d]
+   (map str/join (partition d b))))
+
+(defn select-in-path [p idxs]
+  (for [p p]
+    (str/join (map (vec p) idxs))))
+
+(defn component [^String b ^long idx]
+  (str/join (take-nth (dimension b) (drop idx b))))
+
+(defn components [^String b idxs]
+  (for [i idxs]
+    (component b i)))
+
+;; An example database for this query on domain {0, 1, 2} is R(A, B) =
+;; {(0, 1)}, S(B,C) = {(1, 2)}, T(A, C) = {(0, 2)} and a possible
+;; Booleanisation of the relations could be R(2) (A0, A1, B0, B1) =
+;; {(0, 0, 0, 1)}, S(2) (B0, B1 , C0, C1) = {(0, 1, 1, 0)}, T(2)(A0,
+;; A1, C0, C1) = {(0, 0, 1, 0)}
+
+(comment
+  ;; bits
+  (let [r (interleave-bits [0 1])
+        s (interleave-bits [1 2])
+        t (interleave-bits [0 2])
+        expected-q (interleave-bits [0 1 2])
+
+        find-vars '[A B C]
+        joins [['A [[r 0]
+                    [t 0]]]
+               ['B [[r 1]
+                    [s 0]]]
+               ['C [[s 1]
+                    [t 1]]]]
+
+        result-tuple (for [[_ join] joins
+                           :let [vs (for [[rel col] join]
+                                      (component rel col))]
+                           :while (apply = vs)]
+                       (first vs))
+        result (when (= (count find-vars)
+                        (count result-tuple))
+                 (interleave-bits result-tuple))]
+    [expected-q
+     result
+     (= expected-q result)
+     (->> (range (count find-vars))
+          (components result)
+          (mapv parse-binary-str))
+     find-vars
+     joins
+     (s/conform :crux.datalog/program
+                '[r(0, 1).
+                  s(1, 2).
+                  t(0, 2).
+
+                  q(A, B, C) :- r(A, B), s(B, C), t(A, C).])]))

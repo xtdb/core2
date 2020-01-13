b9a9b96b5803b7fa421b9776c6e1fb98ab3a4d8d
Håkan Råberg <hakan.raberg@gmail.com>
2020-01-13 17:39:27 +0000

    Formatting.

--- examples/arrow-jnr/test/crux/wcoj_test.clj
+++ examples/arrow-jnr/test/crux/wcoj_test.clj
@@ -45,13 +45,13 @@
               fib(1, 1).
 
               fib(N, F) :-
-              N != 0,
-              N != 1,
-              N1 :- -(N, 1),
-              N2 :- -(N, 2),
-              fib(N1, F1),
-              fib(N2, F2),
-              F :- +(F1 F2) .]
+                N != 0,
+                N != 1,
+                N1 :- -(N, 1),
+                N2 :- -(N, 2),
+                fib(N1, F1),
+                fib(N2, F2),
+                F :- +(F1 F2).]
         db (wcoj/execute-datalog fib)
         result (wcoj/query-datalog db 'fib '[30 F])]
     (t/is (= #{[30 832040]} (set result)))))

8c0c0b1f7d118cd2efff8cf561356e31f6fcd254
Håkan Råberg <hakan.raberg@gmail.com>
2020-02-16 17:38:11 +0100

    Slightly more complicated WAL relations, moving replay to read operations and keeping the backing relation via a SoftReference so it can be moved out of memory if not used.

--- examples/arrow-jnr/src/crux/wcoj/wal.clj
+++ examples/arrow-jnr/src/crux/wcoj/wal.clj
@@ -2,59 +2,82 @@
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
             [crux.wcoj :as wcoj])
-  (:import [java.io File FileOutputStream]
+  (:import [java.io File RandomAccessFile]
            java.nio.charset.StandardCharsets
+           java.lang.ref.SoftReference
            java.lang.AutoCloseable))
 
 (defprotocol WAL
   (write [this record])
-  (read-all [this])
+  (read-all [this offset])
   (delete [this]))
 
-(defrecord WALRelation [wal relation]
+(defn- replay-wal [wal offset relation]
+  (reduce (fn [[_ relation] {:keys [record next-offset]}]
+            (let [[op value] record]
+              [next-offset
+               (case op
+                 :insert (wcoj/insert relation value)
+                 :delete (wcoj/delete relation value))]))
+          [offset (or relation (wcoj/*tuple-relation-factory* ""))]
+          (read-all wal (if relation
+                          offset
+                          0))))
+
+(deftype WALRelation [^:volatile-mutable ^long next-offset ^:volatile-mutable ^SoftReference relation wal]
   wcoj/Relation
   (table-scan [this db]
-    (wcoj/table-scan relation db))
+    (let [[next-offset relation] (replay-wal wal next-offset (.get relation))]
+      (set! (.-relation this) (SoftReference. relation))
+      (set! (.-next-offset this) next-offset)
+      (wcoj/table-scan relation db)))
 
   (table-filter [this db var-bindings]
-    (wcoj/table-filter relation db var-bindings))
+    (let [[next-offset new-relation] (replay-wal wal next-offset (.get relation))]
+      (set! (.-relation this) (SoftReference. relation))
+      (set! (.-next-offset this) next-offset)
+      (wcoj/table-filter relation db var-bindings)))
 
   (insert [this value]
-    (-> this
-        (update :wal write [:insert value])
-        (update :relation wcoj/insert value)))
+    (update this :wal write [:insert value]))
 
   (delete [this value]
-    (-> this
-        (update :wal write [:delete value])
-        (update :relation wcoj/delete value)))
+    (update this :wal write [:delete value]))
 
   (truncate [this]
-    (-> this
-        (update :wal delete)
-        (update :relation wcoj/truncate)))
+    (set! (.-relation this) (SoftReference. (some-> relation (wcoj/truncate))))
+    (set! (.-next-offset this) 0)
+    (update :wal delete))
 
   (cardinality [this]
-    (wcoj/cardinality relation))
+    (let [[next-offset relation] (replay-wal wal next-offset (.get relation))]
+      (set! (.-relation this) (SoftReference. relation))
+      (set! (.-next-offset this) next-offset)
+      (wcoj/cardinality relation)))
 
   AutoCloseable
   (close [this]
+    (set! (.-relation this) nil)
+    (set! (.-next-offset this) -1)
     (wcoj/try-close wal)))
 
-(deftype EDNFileWAL [^:volatile-mutable ^FileOutputStream out ^File f sync?]
+(deftype EDNFileWAL [^:volatile-mutable ^RandomAccessFile write-raf ^File f sync?]
   WAL
   (write [this record]
-    (when-not out
-      (set! out (FileOutputStream. f true)))
-    (.write out (.getBytes (prn-str record) StandardCharsets/UTF_8))
-    (when sync?
-      (.sync (.getFD out)))
+    (when-not write-raf
+      (set! (.-write-raf this) (RandomAccessFile. f (if sync? "rwd" "rw"))))
+    (.write write-raf (.getBytes (prn-str record) StandardCharsets/UTF_8))
     this)
 
-  (read-all [this]
-    (when (.exists f)
-      (with-open [reader (io/reader f)]
-        (mapv edn/read-string (line-seq reader)))))
+  (read-all [this offset]
+    (when (and (.exists f) (> (.length f) offset))
+      (with-open [read-raf (doto (RandomAccessFile. f "r")
+                             (.seek offset))]
+        (->> (repeatedly #(when-let [l (.readLine read-raf)]
+                            {:record (read-string l)
+                             :next-offset (.getFilePointer read-raf)}))
+             (take-while some?)
+             (vec)))))
 
   (delete [this]
     (wcoj/try-close this)
@@ -63,8 +86,8 @@
 
   AutoCloseable
   (close [this]
-    (wcoj/try-close out)
-    (set! out nil)))
+    (wcoj/try-close write-raf)
+    (set! (.-write-raf this) nil)))
 
 (defn new-edn-file-wal
   (^crux.wcoj.wal.EDNFileWAL [f]
@@ -73,16 +96,8 @@
    (let [f (io/file f)]
      (->EDNFileWAL nil f sync?))))
 
-(defn- replay-wal [wal relation]
-  (reduce (fn [relation [op value]]
-            (case op
-              :insert (wcoj/insert relation value)
-              :delete (wcoj/delete relation value)))
-          relation
-          (read-all wal)))
-
 (defn new-wal-relation ^crux.wcoj.wal.WALRelation [wal relation-or-name]
   (let [relation (if (string? relation-or-name)
                    (wcoj/*tuple-relation-factory* relation-or-name)
                    relation-or-name)]
-    (->WALRelation wal (replay-wal wal relation))))
+    (->WALRelation 0 (SoftReference. relation) wal)))

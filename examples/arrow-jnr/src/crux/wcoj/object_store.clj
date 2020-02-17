f25a5cbf8d800b9e0e182b3811ed6beb8abb4c2c
Håkan Råberg <hakan.raberg@gmail.com>
2020-02-17 17:04:43 +0100

    Steps towards a file cache.

--- examples/arrow-jnr/src/crux/wcoj/object_store.clj
+++ examples/arrow-jnr/src/crux/wcoj/object_store.clj
@@ -4,6 +4,8 @@
   (:import java.io.File
            [java.nio.file Files FileVisitResult SimpleFileVisitor]
            java.nio.file.attribute.FileAttribute
+           [java.lang.ref Cleaner WeakReference]
+           [java.util HashMap LinkedHashMap Map]
            java.lang.AutoCloseable))
 
 (set! *unchecked-math* :warn-on-boxed)
@@ -34,10 +36,11 @@
 
   (delete-object [this k]
     (let [f (io/file dir k)]
-      (.delete f)
-      (when-let [parent (.getParentFile f)]
-        (when (empty? (.listFiles parent))
-          (.delete parent))))
+      (when (.isFile f)
+        (.delete f)
+        (when-let [parent (.getParentFile f)]
+          (when (empty? (.listFiles parent))
+            (.delete parent)))))
     nil)
 
   AutoCloseable
@@ -73,14 +76,36 @@
 (defn- create-tmpdir ^java.io.File [dir-name]
   (.toFile (Files/createTempDirectory dir-name (make-array FileAttribute 0))))
 
+(defonce ^:private ^Cleaner cleaner (Cleaner/create))
+
 (declare evict-object)
 
-(deftype CachedObjectStore [^LocalDirectoryObjectStore object-store-cache object-store]
+(deftype CachedObjectStore [^Map cold-map ^LinkedHashMap lru-cache
+                            ^LocalDirectoryObjectStore object-store-cache object-store
+                            ^long size-limit]
   ObjectStore
   (get-object [this k]
-    (or (get-object object-store-cache k)
-        (when-let [v (get-object object-store k)]
-          (put-object object-store-cache k v))))
+    (or (.get lru-cache k)
+        (when-let [v-url (or (some-> ^WeakReference (.get cold-map k) (.get))
+                             (when-let [source-v-url (get-object object-store k)]
+                               (let [v-url (with-open [in (io/input-stream source-v-url)]
+                                             (put-object object-store-cache k in))
+                                     dir-size (dir-size (.dir object-store-cache))
+                                     load-factor 0.75]
+                                 (when (> dir-size size-limit)
+                                   (loop [dir-size dir-size
+                                          vs (.iterator (.values lru-cache))]
+                                     (when (and (> dir-size (* load-factor size-limit))
+                                                (.hasNext vs))
+                                       (let [expired-v (.next vs)]
+                                         (recur (- dir-size (.length (io/file expired-v)))
+                                                (doto vs
+                                                  (.remove)))))))
+                                 (.register cleaner v-url #(evict-object this k))
+                                 (.put cold-map k (WeakReference. v-url))
+                                 v-url)))]
+          (.put lru-cache k v-url)
+          (.get lru-cache k))))
 
   (put-object [this k v]
     (evict-object this k)
@@ -98,10 +123,16 @@
     (delete-dir (.dir object-store-cache))))
 
 (defn- evict-object [^CachedObjectStore cached-object-store k]
-  (delete-object (.object-store-cache cached-object-store) k))
+  (delete-object (.object-store-cache cached-object-store) k)
+  (.remove ^Map (.cold-map cached-object-store) k)
+  (.remove ^Map (.lru-cache cached-object-store) k))
 
 (defn new-cached-object-store
-  (^crux.wcoj.object_store.CachedObjectStore [object-store]
-   (new-cached-object-store (create-tmpdir "cached_object_store") object-store))
-  (^crux.wcoj.object_store.CachedObjectStore [cache-dir object-store]
-   (->CachedObjectStore (new-local-directory-object-store cache-dir) object-store)))
+  (^crux.wcoj.object_store.CachedObjectStore [object-store size-limit]
+   (new-cached-object-store (create-tmpdir "cached_object_store") object-store size-limit))
+  (^crux.wcoj.object_store.CachedObjectStore [cache-dir object-store size-limit]
+   (->CachedObjectStore (HashMap.)
+                        (LinkedHashMap. 16 0.75 true)
+                        (new-local-directory-object-store cache-dir)
+                        object-store
+                        size-limit)))

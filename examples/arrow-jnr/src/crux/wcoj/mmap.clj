c93646a36a880d969e337c8665ab8b9bb9d24783
Håkan Råberg <hakan.raberg@gmail.com>
2020-02-18 22:12:39 +0100

    Simplify mmap caching.

--- examples/arrow-jnr/src/crux/wcoj/mmap.clj
+++ examples/arrow-jnr/src/crux/wcoj/mmap.clj
@@ -6,7 +6,6 @@
            [java.nio ByteBuffer MappedByteBuffer]
            [java.nio.channels FileChannel$MapMode SeekableByteChannel]
            [java.util HashMap LinkedHashMap Map Map$Entry]
-           [java.lang.ref Cleaner WeakReference]
            io.netty.util.internal.PlatformDependent))
 
 (defn new-byte-buffer-seekable-byte-channel ^java.nio.channels.SeekableByteChannel [^ByteBuffer buffer]
@@ -47,19 +46,18 @@
 (defn unmap-buffer [^MappedByteBuffer buffer]
   (PlatformDependent/freeDirectBuffer buffer))
 
-(defonce ^:private ^Cleaner cleaner (Cleaner/create))
-
 (defrecord MmapPool [^Map buffer-cache ^Map url-cache object-store])
 
 (defn new-mmap-pool [object-store size]
-  (->MmapPool (proxy [LinkedHashMap] [size 0.75 true]
-                (removeEldestEntry [^Map$Entry entry]
-                  (if (> (count this) size)
-                    (do (unmap-buffer (.getValue entry))
-                        true)
-                    false)))
-              (HashMap.)
-              object-store))
+  (let [buffer-cache (HashMap.)]
+    (->MmapPool buffer-cache
+                (proxy [LinkedHashMap] [size 0.75 true]
+                  (removeEldestEntry [^Map$Entry entry]
+                    (if (> (count this) size)
+                      (do (unmap-buffer (.remove ^Map buffer-cache (.getValue entry)))
+                          true)
+                      false)))
+                object-store)))
 
 (defn- get-file-url ^java.net.URL [^MmapPool mmap-pool k]
   (when-let [url ^URL (wcoj-os/get-object (.object-store mmap-pool) k)]
@@ -67,19 +65,14 @@
       (throw (IllegalArgumentException. (str "Not a file:" url)))
       url)))
 
-(defn- mmap-url-ref ^java.nio.MappedByteBuffer [^MmapPool mmap-pool ^WeakReference url-ref]
-  (let [buffer (mmap-file (io/file (.get ^WeakReference url-ref)))]
-    (.put ^Map (.buffer-cache mmap-pool) url-ref buffer)
-    buffer))
+(defn- mmap-file-url ^java.nio.MappedByteBuffer [^MmapPool mmap-pool url]
+  (doto (mmap-file (io/file url))
+    (->> (.put ^Map (.buffer-cache mmap-pool) url))))
 
 (defn mmap-object ^java.nio.MappedByteBuffer [^MmapPool mmap-pool k]
-  (if-let [url-ref (.get ^Map (.url-cache mmap-pool) k)]
-    (or (.get ^Map (.buffer-cache mmap-pool) url-ref)
-        (mmap-url-ref mmap-pool url-ref))
+  (if-let [url (.get ^Map (.url-cache mmap-pool) k)]
+    (or (.get ^Map (.buffer-cache mmap-pool) url)
+        (mmap-file-url mmap-pool url))
     (when-let [url (get-file-url mmap-pool k)]
-      (let [url-ref (WeakReference. url)]
-        (.register cleaner url #(unmap-buffer
-                                 (.remove ^Map (.buffer-cache mmap-pool)
-                                          (.remove ^Map (.url-cache mmap-pool) k))))
-        (.put ^Map (.url-cache mmap-pool) k url-ref)
-        (mmap-url-ref mmap-pool url-ref)))))
+      (.put ^Map (.url-cache mmap-pool) k url)
+      (mmap-file-url mmap-pool url))))

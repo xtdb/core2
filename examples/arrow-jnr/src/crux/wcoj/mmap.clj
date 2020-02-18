8d9fd7fde74c3f35fdf82fe40e73a00657c7d796
Håkan Råberg <hakan.raberg@gmail.com>
2020-02-18 20:59:33 +0100

    Initial stab at the mmap pool, not working.

--- examples/arrow-jnr/src/crux/wcoj/mmap.clj
+++ examples/arrow-jnr/src/crux/wcoj/mmap.clj
@@ -1,8 +1,12 @@
 (ns crux.wcoj.mmap
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [crux.wcoj.object-store :as wcoj-os])
   (:import java.io.FileInputStream
+           java.net.URL
            [java.nio ByteBuffer MappedByteBuffer]
            [java.nio.channels FileChannel$MapMode SeekableByteChannel]
+           [java.util HashMap LinkedHashMap Map]
+           [java.lang.ref Cleaner WeakReference]
            io.netty.util.internal.PlatformDependent))
 
 (defn new-byte-buffer-seekable-byte-channel ^java.nio.channels.SeekableByteChannel [^ByteBuffer buffer]
@@ -42,3 +46,36 @@
 
 (defn unmap-buffer [^MappedByteBuffer buffer]
   (PlatformDependent/freeDirectBuffer buffer))
+
+(defonce ^:private ^Cleaner cleaner (Cleaner/create))
+
+(defrecord MmapPool [^Map buffer-cache ^Map url-cache object-store])
+
+(defn new-mmap-pool [object-store size]
+  (->MmapPool (proxy [LinkedHashMap] [size 0.75 true]
+                (removeEldestEntry [_]
+                  (> (count this) size)))
+              (HashMap.)
+              object-store))
+
+(defn- get-file-url ^java.net.URL [mmap-pool k]
+  (when-let [url ^URL (wcoj-os/get-object mmap-pool k)]
+    (if-not (= "file" (.getProtocol url))
+      (throw (IllegalArgumentException. (str "Not a file:" url)))
+      url)))
+
+(defn mmap-object ^java.nio.MappedByteBuffer [^MmapPool mmap-pool k]
+  (if-let [url-ref (.get ^Map (.url-cache mmap-pool) k)]
+    (or (.get ^Map (.buffer-cache mmap-pool) url-ref)
+        (let [buffer (mmap-file (io/file (.get ^WeakReference url-ref)))]
+          (.put ^Map (.buffer-cache mmap-pool) url-ref buffer)
+          buffer))
+    (when-let [url (get-file-url mmap-pool k)]
+      (let [buffer (mmap-file (io/file url))
+            url-ref (WeakReference. url)]
+        (.register cleaner url #(unmap-buffer
+                                 (.remove ^Map (.buffer-cache mmap-pool)
+                                          (.remove ^Map (.url-cache mmap-pool) k))))
+        (.put ^Map (.url-cache mmap-pool) k url-ref)
+        (.put ^Map (.buffer-cache mmap-pool) url-ref buffer)
+        buffer))))

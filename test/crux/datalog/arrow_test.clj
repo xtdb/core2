cb2ec315597d62a737212b7d1114d9744ce20435
Håkan Råberg <hakan.raberg@gmail.com>
2020-03-18 12:04:03 +0100

    Test for memory leaks.

---
+++ test/crux/datalog/arrow_test.clj
@@ -0,0 +1,56 @@
+(ns crux.datalog.arrow-test
+  (:require [clojure.test :as t]
+            [crux.datalog :as d]
+            [crux.datalog.arrow :as da])
+  (:import org.apache.arrow.memory.RootAllocator
+           java.lang.AutoCloseable))
+
+(declare try-reclaim-memory)
+
+(t/deftest test-memory-consumption
+  (binding [d/*tuple-relation-factory* #'da/new-arrow-struct-relation
+            da/*allocator* (RootAllocator.)]
+    (t/is (zero? (.getAllocatedMemory da/*allocator*)))
+
+    (t/testing "db memory usage"
+      (let [edge '[edge(1, 2).
+                   edge(2, 3).
+
+                   path(X, Y) :- edge(X, Y).
+                   path(X, Z) :- path(X, Y), edge(Y, Z).]
+            db (d/execute edge)]
+        (t/is (pos? (.getAllocatedMemory da/*allocator*)))
+        (d/close-db db)
+        (t/is (zero? (.getAllocatedMemory da/*allocator*)))))
+
+    ;; TODO: Fix memory leaks during query.
+    #_(t/testing "query memory usage"
+        (let [edge '[edge(1, 2).
+                     edge(2, 3).
+
+                     path(X, Y) :- edge(X, Y).
+                     path(X, Z) :- path(X, Y), edge(Y, Z).]
+              db (d/execute edge)
+              db-memory (.getAllocatedMemory da/*allocator*)
+              result (set (d/query-by-name db 'path))
+              query-memory (.getAllocatedMemory da/*allocator*)]
+          (t/is (>= query-memory db-memory))
+          (t/is (= #{[1 2] [2 3] [1 3]} result))
+
+          (t/testing "reclaiming memory"
+            (try-reclaim-memory)
+            (t/is (= db-memory (.getAllocatedMemory da/*allocator*))))
+
+          (t/testing "closing db frees remaining memory"
+            (d/close-db db)
+            (t/is (zero? (.getAllocatedMemory da/*allocator*))))))
+
+    (t/testing "can cleanly close allocator"
+      (.close da/*allocator*))))
+
+(defn- try-reclaim-memory []
+  (loop [memory (.getAllocatedMemory da/*allocator*)]
+    (System/gc)
+    (let [new-memory (.getAllocatedMemory da/*allocator*)]
+      (when (< new-memory memory)
+        (recur new-memory)))))

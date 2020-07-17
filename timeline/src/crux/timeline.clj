dd712ac544b28c1d3c3a2d716c721fda7349b785
Håkan Råberg <hakan.raberg@gmail.com>
2020-07-17 19:12:15 +0200

    Adding TODO.

--- timeline/src/crux/timeline.clj
+++ timeline/src/crux/timeline.clj
@@ -504,6 +504,9 @@
 
 ;; See example in figure 4.1 in abadi-column-stores.pdf page 49 / 242
 
+;; TODO: implement ripple updates and cutter joins from
+;;       StratosIdreosDBcrackingThesis.pdf
+
 ;; Potentially useful for incremental index maintenance.
 
 (defn upper-int ^long [^long x]

d7b7b036e25274a10c7326875810b1020410967b
Steven Deobald <steven@deobald.ca>
2022-08-22 19:35:26 -0300

    [ci skip] Correct the local open script for MacOS

--- doc/adoc/bin/open-local-site.sh
+++ doc/adoc/bin/open-local-site.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-URL="file://$(pwd)/build/site/index.html"
+URL="file://$(pwd)/build/site/main/index.html"
 
 [[ -x $BROWSER ]] && exec "$BROWSER" "$URL"
 
-path=$(which xdg-open || which gnome-open) && exec "$path" "$URL"
+path=$(which xdg-open || which gnome-open || which open) && exec "$path" "$URL"
 
 echo "Couldn't open browser, here's the URL to open:"
 echo $URL

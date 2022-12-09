5982c99d1ffb910de5c078c9bafb47f840dc683a
Steven Deobald <steven@deobald.ca>
2022-12-08 20:37:09 -0400

    Quick update to handle multiple 'railroad::' blocks
    * this PR is probably no longer WIP, unless we want a different
      strategy for embedding railroad diagrams

--- doc/adoc/antora-extensions/railroads.js
+++ doc/adoc/antora-extensions/railroads.js
@@ -1,30 +1,41 @@
 const fs = require('fs');
+const matcher = /railroad::(?<filename>.*)\[\]/;
+
+function embed(s, filename) {
+  var path = './railroads/'.concat(filename);
+  var contents;
+
+  try {
+    contents = fs.readFileSync(path, 'utf8');
+//    console.log(contents);
+  } catch (err) {
+    console.error(err);
+  }
+
+  return s.replace(matcher, contents);
+}
+
+function tryEmbed(s) {
+  var svg = s.match(matcher);
+  if (svg != null) {
+    return embed(s, svg.groups.filename);
+  } else {
+    return s;
+  }
+}
 
 module.exports = function (registry) {
   registry.postprocessor('railroad', function () {
-    console.log('postprocessor');
+//    console.log('postprocessor');
     var self = this
     self.process(function (doc, output) {
       var proc = doc.attributes.$$smap.docfile;
-      console.log('processing:',proc);
+//      console.log('processing:',proc);
 
-      // TODO: make this match multiple railroad:: blocks
-      var matcher = /railroad::(?<filename>.*)\[\]/;
       var svg = output.match(matcher);
-
       if (svg != null) {
-        console.log('svg is:',svg.groups.filename);
-        var path = './railroads/'.concat(svg.groups.filename);
-        var contents;
-
-        try {
-          contents = fs.readFileSync(path, 'utf8');
-          console.log(contents);
-        } catch (err) {
-          console.error(err);
-        }
-
-        return output.replace(matcher, contents);
+        console.log('first matched railroad is:', svg.groups.filename);
+        return output.split('\n').map(line => tryEmbed(line)).join('\n');
       } else {
         return output;
       }

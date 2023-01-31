96ec64d6b16c425bc8988e1ccb40c1492dad6042
Matt Butler <mattb-w@hotmail.co.uk>
2023-01-31 19:47:32 +0000

    Add versions of select to scan rules for 3 arity scan

--- core/src/core2/logical_plan.clj
+++ core/src/core2/logical_plan.clj
@@ -776,46 +776,66 @@
      [:select predicate-2
       [:scan table relation]]]
     ;;=>
-    [:select (merge-conjunctions predicate-1 predicate-2) [:scan table relation]]))
+    [:select (merge-conjunctions predicate-1 predicate-2) [:scan table relation]]
+
+    [:select predicate-1
+     [:select predicate-2
+      [:scan src table relation]]]
+    ;;=>
+    [:select (merge-conjunctions predicate-1 predicate-2) [:scan src table relation]]))
+
+(defn- try-add-predicate-to-scan-columns [predicate columns]
+  (let [underlying-scan-columns (set (map ->projected-column columns))]
+        (reduce
+          (fn [{:keys [scan-columns new-select-predicate] :as acc} predicate]
+            (let [expr-columns-also-in-scan-columns
+                  (set/intersection underlying-scan-columns
+                                    (set (flatten (if (coll? predicate)
+                                                    (seq predicate)
+                                                    [predicate]))))]
+              (if-let [single-column (when (= 1 (count expr-columns-also-in-scan-columns))
+                                       (first expr-columns-also-in-scan-columns))]
+                {:scan-columns
+                 (vec (for [column-or-select-expr scan-columns
+                            :let [column (->projected-column column-or-select-expr)]]
+                        (if (= single-column column)
+                          (if (extend-projection? column-or-select-expr)
+                            (update column-or-select-expr column (partial merge-conjunctions predicate))
+                            {column predicate})
+                          column-or-select-expr)))
+                 :new-select-predicate new-select-predicate}
+                (update
+                  acc
+                  :new-select-predicate
+                  #(if %
+                     (merge-conjunctions % predicate)
+                     predicate)))))
+          {:scan-columns columns :new-select-predicate nil}
+          (conjunction-clauses predicate))))
 
 (defn- add-selection-to-scan-predicate [z]
   (r/zmatch z
     [:select predicate
      [:scan table columns]]
     ;;=>
-    (let [underlying-scan-columns (set (map ->projected-column columns))
-          {:keys [scan-columns new-select-predicate]}
-          (reduce
-            (fn [{:keys [scan-columns new-select-predicate] :as acc} predicate]
-              (let [expr-columns-also-in-scan-columns
-                    (set/intersection underlying-scan-columns
-                                      (set (flatten (if (coll? predicate)
-                                                      (seq predicate)
-                                                      [predicate]))))]
-                (if-let [single-column (when (= 1 (count expr-columns-also-in-scan-columns))
-                                         (first expr-columns-also-in-scan-columns))]
-                  {:scan-columns
-                   (vec (for [column-or-select-expr scan-columns
-                              :let [column (->projected-column column-or-select-expr)]]
-                          (if (= single-column column)
-                            (if (extend-projection? column-or-select-expr)
-                              (update column-or-select-expr column (partial merge-conjunctions predicate))
-                              {column predicate})
-                            column-or-select-expr)))
-                   :new-select-predicate new-select-predicate}
-                  (update
-                    acc
-                    :new-select-predicate
-                    #(if %
-                       (merge-conjunctions % predicate)
-                       predicate)))))
-            {:scan-columns columns :new-select-predicate nil}
-            (conjunction-clauses predicate))]
+    (let [{:keys [scan-columns new-select-predicate]}
+          (try-add-predicate-to-scan-columns predicate columns)]
       (when-not (= columns scan-columns)
         (if new-select-predicate
           [:select new-select-predicate
            [:scan table scan-columns]]
-          [:scan table scan-columns])))))
+          [:scan table scan-columns])))
+
+    [:select predicate
+     [:scan src table columns]]
+    ;;=>
+    (let [{:keys [scan-columns new-select-predicate]}
+          (try-add-predicate-to-scan-columns predicate columns)]
+      (when-not (= columns scan-columns)
+        (if new-select-predicate
+          [:select new-select-predicate
+           [:scan src table scan-columns]]
+          [:scan src table scan-columns])))))
 
 ;; Decorrelation rules.
 
